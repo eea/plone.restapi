6152371018d746c8619719b40aeadee704540c5a
Alin Voinea <contact@avoinea.com>
2020-07-02 16:48:19 +0300

    Update merge script

--- merge_branches.sh
+++ merge_branches.sh
@@ -6,6 +6,7 @@ git remote add upstream https://github.com/plone/plone.restapi.git
 
 git fetch --all
 git pull
-git merge upstream/master
-git merge origin/improve_portlets
+git merge --no-ff origin/improve_portlets
+git merge --no-ff upstream/master
+git merge --no-ff upstream/eea-dx-cpanel-metadata
 # git merge origin/export_components
